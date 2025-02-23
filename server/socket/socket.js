14cb360e49ef3da3bcd1557dc4eae9856d4b3ea4
kartikpanganti <kartikpanganti@gmail.com>
2025-02-23 21:51:03 +0530

    create socket.js and maintaining the seperate socket folder

---
+++ server/socket/socket.js
@@ -0,0 +1,31 @@
+import { Server } from "socket.io";
+
+export const initializeSockets = (server) => {
+  const io = new Server(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST", "PUT", "DELETE"],
+    },
+  });
+
+  console.log("✅ Socket.IO Initialized");
+
+  io.on("connection", (socket) => {
+    console.log(`🟢 Socket Connected: ${socket.id}`);
+   
+    // Handle Custom Events
+    socket.on("custom-event", (data) => {
+      console.log(`Custom Event Received: ${data}`);
+    });
+
+
+
+
+    // Handle Disconnection
+    socket.on("disconnect", () => {
+      console.log(`🔴 Socket Disconnected: ${socket.id}`);
+    });
+  });
+
+  return io;
+};
